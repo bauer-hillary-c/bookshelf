353ec2ea58c2b803dbaea5aad87bc603a4ff6223
bauer-hillary-c <hbauer@ezcater.com>
2018-02-06 23:01:18 -0500

    adds deleteAuthor function

---
+++ app/graphql/mutations/delete_author.rb
@@ -0,0 +1,11 @@
+class Mutations::DeleteAuthor < GraphQL::Function
+
+  argument :id, types.ID
+
+  type types.Boolean
+
+  def call(obj, args, ctx)
+    author = Author.find(args[:id])
+    !!author.destroy
+  end
+end
