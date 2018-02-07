59b6246a072ccc931f3a79d5e38ce3942f0a319e
bauer-hillary-c <hbauer@ezcater.com>
2018-02-06 22:19:16 -0500

    pulls out createAuthor mutation to a function

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,17 +1,5 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :createAuthor, Types::AuthorType do
-    argument :first_name, types.String
-    argument :last_name, types.String
-    argument :birth_year, types.Int
-    argument :is_alive, types.Boolean
-
-    description "creates an author"
-
-    resolve ->(obj, args, ctx) {
-      Author.create(args.to_h)
-    }
-  end
+  field :createAuthor, function: Mutations::CreateAuthor.new
 end
